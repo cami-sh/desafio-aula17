71bf9c3cd75af92d667e527ef93f696b45d0ba47
Camila Sayuri <camilahanazono@gmail.com>
2021-01-29 21:32:00 -0300

    requisições e listagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import {User} from "./types/User"
+import {Post} from "./types/Post"
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  const [selectID, setSelectID] = useState<number>(0)
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users/')
+    .then(resposta => setUsers(resposta.data))
+  }, [])
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${selectID}/posts`)
+    .then(resposta => setPosts(resposta.data))
+  }, [selectID])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <ul>
+        {
+          users != null &&
+          users.map((user:User) => (
+            <li key={user.id} onClick={() => setSelectID(user.id)}>
+              {user.name} ({user.id})
+            </li>
+          ))
+        }
+      </ul>
+
+        {
+          posts != null &&
+          posts.map((post:Post) => (
+            <div key={post.id}>
+              <h2>{post.userId} - {post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))
+        }
+
+
     </div>
   );
 }
